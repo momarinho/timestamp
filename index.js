2981f56715f745ec5a00081dd9087b52135b06d4
momarinho <117489486+momarinho@users.noreply.github.com>
2023-02-18 18:54:52 +0000

    finish

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-app.get('/api/:date?', (req, res) => {
-  const dateString = req.params.date;
-  const dateStringRegex = /^[0-9]+$/;
-  const numbersOnly = dateStringRegex.test(dateString);
+const express = require('express');
+const cors = require('cors');
 
-  if (!numbersOnly) {
-    const unixTimestamp = Date.parse(dateString);
-    const utcDate = new Date(unixTimestamp).toUTCString();
+const app = express();
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('public'));
 
-    unixTimestamp
-      ? res.json({ unix: unixTimestamp, utc: utcDate })
-      : res.json({ error: 'Invalid Date' });
-  } else {
-    const unixTimestamp = parseInt(dateString);
-    const actualDate = new Date(unixTimestamp);
-    const utcDate = actualDate.toUTCString();
-
-    res.json({ unix: unixTimestamp, utc: utcDate });
-  }
-
-  app.get('/api', (req, res) => {
-    const currentDate = new Date().toUTCString();
-    const currentUnix = Date.parse(currentDate);
-    res.json({ unix: currentUnix, utc: currentDate });
-  });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+app.get('/api/hello', (req, res) => {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api', (req, res) => {
+  const currentDate = new Date();
+  res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
+});
+
+app.get('/api/:date?', (req, res) => {
+  const { date } = req.params;
+  let dateObj;
+
+  if (!date) {
+    dateObj = new Date();
+  } else if (!isNaN(date)) {
+    dateObj = new Date(parseInt(date));
+  } else {
+    dateObj = new Date(date);
+  }
+
+  if (isNaN(dateObj.getTime())) {
+    res.json({ error: 'Invalid Date' });
+  } else {
+    res.json({ unix: dateObj.getTime(), utc: dateObj.toUTCString() });
+  }
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
